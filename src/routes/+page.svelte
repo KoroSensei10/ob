f3be6e8a7a1ddc76e16c37b259a02b9a7601003c
Mathis <mathisjung02@gmail.com>
2025-08-17 00:44:44 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
+
+  let name = $state("");
+  let greetMsg = $state("");
+
+  async function greet(event: Event) {
+    event.preventDefault();
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsg = await invoke("greet", { name });
+  }
+</script>
+
+<main class="w-screen h-screen bg-black text-white">
+  data!
+  <button onclick={greet} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Click me
+  </button>
+  <p>{greetMsg}</p>
+</main>
+
