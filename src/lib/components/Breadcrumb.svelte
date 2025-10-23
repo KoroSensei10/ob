4a6f46447493736a3e36d80ec59bd125baaedd38
Mathis <mathisjung02@gmail.com>
2025-10-23 14:37:53 +0200

    fix breadcrumb size

--- src/lib/components/Breadcrumb.svelte
+++ src/lib/components/Breadcrumb.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
-    import { ChevronRight } from '@lucide/svelte';
-    import { openFilesStore } from '$stores/OpenFiles.svelte';
+	import { ChevronRight } from '@lucide/svelte';
+	import { openFilesStore } from '$stores/OpenFiles.svelte';
 
-
-    let activeFilePath = $derived(
-    	openFilesStore.activeFile?.path.split('/') ?? [],
-    );
-    let nbSegment = $derived(activeFilePath?.length);
+	let activeFilePath = $derived(
+		openFilesStore.activeFile?.path.split('/') ?? [],
+	);
+	let nbSegment = $derived(activeFilePath?.length);
 </script>
 
 {#if activeFilePath.length}
-    <div class="flex gap-1 px-2 text-sm items-center text-gray-400">
-        {#each activeFilePath || '' as segment, index (index)}
-            {#if index !== nbSegment - 1}
-                <span class="text-gray-400">{segment}</span>
-                <span><ChevronRight strokeWidth={1} /></span>
-            {:else}
-                <span class="text-gray-200">{segment}</span>
-            {/if}
-        {/each}
-    </div>
+	<div class="flex gap-1 px-2 text-sm items-center text-gray-400">
+		{#each activeFilePath || '' as segment, index (index)}
+			{#if index !== nbSegment - 1}
+				<span class="text-gray-400">{segment}</span>
+				<span><ChevronRight class="w-4 h-4" strokeWidth={1} /></span>
+			{:else}
+				<span class="text-gray-200">{segment}</span>
+			{/if}
+		{/each}
+	</div>
 {/if}
