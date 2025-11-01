4b0a9354942fa4a0805f17b3747c1b69a80dd066
Mathis <mathisjung02@gmail.com>
2025-11-01 17:38:32 +0100

    msic test/issues

--- e2e/(app)/notes/delete.test.ts
+++ e2e/(app)/notes/delete.test.ts
@@ -13,14 +13,14 @@ test.describe('Delete', () => {
 		await expect(page.getByTestId(`file-tree-entry-${noteName}`)).not.toBeAttached();
 	});
 
-	test('Delete note - cancel', async ({ page, createNote }) => {
+	test.fixme('Delete note - cancel', async ({ page, createNote }) => {
 		const { noteName } = createNote;
 		// Attempt to delete the created note but cancel
 		await page.getByTestId(`file-tree-entry-${noteName}`).click({
 			button: 'right'
 		});
 		await page.getByTestId('delete-entry-button').click();
-
+		// Test can pass sometimes because the network may be slow
 		await expect(page.getByTestId(`file-tree-entry-${noteName}`)).toBeAttached();
 	});
 });
