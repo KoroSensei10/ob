cab020bfddf3c9315f473d5a20111219a4975534
Mathis <mathisjung02@gmail.com>
2025-11-05 21:16:20 +0100

    create file with search bar action

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -3,53 +3,101 @@
 	import * as Command from '$lib/components/ui/command';
 	import { getFileTree } from '$lib/remotes/files.remote';
 	import type { FileEntry, FileTree } from '$types/files';
+	import { computeCommandScore } from 'bits-ui';
 
 	interface Props {
 		searchBarOpen: boolean;
 	}
-	let {
-		searchBarOpen = $bindable(),
-	}: Props = $props();
+	let { searchBarOpen = $bindable() }: Props = $props();
 
 	let open = $derived(searchBarOpen);
-	let files = $derived(flattenFiles((await getFileTree()) ?? []));
+	let flattenFiles = $derived(toFlattenFiles(await getFileTree()) ?? []);
+	let files = $derived(flattenFiles);
+
+	let query = $state('');
+	let queryInFiles = $derived(
+		flattenFiles.some(
+			(file) => file.path.toLowerCase() === query.toLowerCase(),
+		),
+	);
 
 	// TODO: move to a utility file
-	function flattenFiles(files: FileTree[], parentPath: string = ''): FileEntry[] {
+	function toFlattenFiles(
+		files: FileTree[],
+		parentPath: string = '',
+	): FileEntry[] {
 		let flatList: FileEntry[] = [];
 		for (const file of files) {
 			const currentPath = parentPath ? `${parentPath}/${file.name}` : file.name;
 			if (file.type === 'file') {
 				flatList.push({ ...file, path: currentPath });
 			} else if (file.type === 'dir' && file.childs) {
-				flatList = flatList.concat(flattenFiles(file.childs, currentPath));
+				flatList = flatList.concat(toFlattenFiles(file.childs, currentPath));
 			}
 		}
 		return flatList;
 	}
+
+	function filter(
+		commandValue: string,
+		search: string,
+		commandKeywords?: string[],
+	): number {
+		if (commandValue === '__new_file__') {
+			// Always show the "Create new file" option
+			return 0.0001;
+		}
+		const score = computeCommandScore(commandValue, search, commandKeywords);
+
+		return score;
+	}
 </script>
 
-<Command.Dialog bind:open>
-	<Command.Input placeholder="Search a file" />
+<Command.Dialog bind:open {filter}>
+	<Command.Input bind:value={query} placeholder="Search a file" />
 	<Command.List>
-		<Command.Empty>No results found.</Command.Empty>
 		<Command.Group heading="Files">
 			{#each files as entry (entry.path)}
 				{#if entry.type === 'file'}
-					<Command.Item onSelect={() => {
-						open = false;
-						coreAPI.openFile(entry);
-					}}>
-						{entry.name}
+					<Command.Item
+						value={entry.path}
+						onSelect={() => {
+							open = false;
+							coreAPI.openFile(entry);
+						}}
+					>
+						<div class="flex justify-between w-full">
+							<span>
+								{entry.name}
+							</span>
+							<span>
+								{#if entry.path.indexOf('/') !== -1}
+									<span class="text-sm text-gray-500 italic">{entry.path}</span>
+								{/if}
+							</span>
+						</div>
 					</Command.Item>
 				{/if}
 			{/each}
 		</Command.Group>
-		<!-- <Command.Separator />
-		<Command.Group heading="Settings">
-			<Command.Item>Profile</Command.Item>
-			<Command.Item>Billing</Command.Item>
-			<Command.Item>Settings</Command.Item>
-		</Command.Group> -->
+		{#if query}
+			<Command.Separator />
+			<Command.Group heading="Actions">
+				<Command.Item
+					class="font-medium mb-2"
+					value="__new_file__"
+					disabled={queryInFiles}
+					onSelect={() => {
+						open = false;
+						coreAPI.files.createAndOpenFile(query);
+					}}
+				>
+					<span> Create File: </span>
+					<span class="font-bold">
+						{query}
+					</span>
+				</Command.Item>
+			</Command.Group>
+		{/if}
 	</Command.List>
 </Command.Dialog>
